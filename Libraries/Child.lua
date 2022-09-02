5b2263630afafef22a1e0d60cabcb455c1a1bca9
ZSI <89996056+ZSIOffical@users.noreply.github.com>
2022-09-02 17:53:34 +0200

    Update Child.lua

--- Libraries/Child.lua
+++ Libraries/Child.lua
@@ -1,14 +1,23 @@
-function childAdded(Path,Callback)
+local child = {}
+function child:childAdded(info)
+    info = info or {}
+    local Path = info.Path
+    local Callback = info.Callback
     game[Path].ChildAdded:connect(function (Part)
         pcall(Callback)
     end)
 end
-function childRemoved(Path,Callback)
+function child:childRemoved(info)
+    info = info or {}
+    local Path = info.Path
+    local Callback = info.Callback
     game[Path].ChildRemoved:connect(function (Part)
         pcall(Callback)
     end)
 end
-function Changed(Path,Callback)
+function child:Changed(info)
+    local Path = info.Path
+    local Callback = info.Callback
     game[Path].Changed:connect(function (Part)
         pcall(Callback)
     end)
