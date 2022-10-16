c2da2876b3f445358d261d6199509780d75798e3
ZSI <89996056+ZSIOffical@users.noreply.github.com>
2022-10-16 12:56:26 +0200

    Update Spamm.lua

--- Discord/Functions/Spamm.lua
+++ Discord/Functions/Spamm.lua
@@ -2,12 +2,31 @@ math.randomseed(os.time())
 local WebHookTable = {}
 local Request = (syn and syn.request) or (http and http.request) or http_request
 local randomWait = math.random(1)
-WebHookTable.Spamm = function(Webhook,Message)
-    if Webhook ~= "" then
+WebHookTable.Spamm = function(Type2,Webhook,Message)
+    if Webhook ~= "" and Type2 ~= "" then
         while true do
-            local Content = {
-                ["content"] = Message
-            },
+            local Content;
+            if Type2 == "Message"
+                Content = {
+                    ["content"] = Message
+                }
+            elseif Type2 == "Embed" then
+                Content = {
+                    ["content"] = Message.Content 
+                    ["embeds"] = {{
+                        ["color"] = Message.Color,
+                        ["title"] = Message.Title,
+                        ["description"] = Message.Description,
+                        ["footer"] = {
+                            ["text"] = Message.Footer 
+                        },
+                        ["author"] = {
+                            ["name"] = Message.Author,
+                            ["icon_url"] = Message.Icon
+                        }
+                    }}
+                }
+            end
             local respone = Request({
                 Url = Webhook,
                 Method = "POST",
