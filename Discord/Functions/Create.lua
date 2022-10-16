1e2ace24e62c39444b215082f4f122f07046cd3d
ZSI <89996056+ZSIOffical@users.noreply.github.com>
2022-10-16 13:43:47 +0200

    Create Create.lua

---
+++ Discord/Functions/Create.lua
@@ -0,0 +1,30 @@
+local WebHookTable = {}
+local Request = (syn and syn.request) or (http and http.request) or http_request
+WebHookTable.Create = function(Token,Id)
+    function CheckUser()
+        local k = Request({
+            Url = "https://discord.com/app",
+            Headers = {["Authorization"] = Token}})
+        if k.StatusCode ~= 200 then
+            print("Invalid token or token is logged")
+            return false
+        elseif k.StatusCode == 200 then
+            return true
+        end
+    end
+    if CheckUser() then
+        local Channel = "https://discord.com/api/v9/channels/"..Id.."/webhooks"
+        local Answer = Request({
+            Url = Channel,
+            Headers = {
+                ["Authorization"] = Token
+            },
+            Method = "POST"
+        })
+        if Answer.StatusCode ~= 200 then
+            return print("Something went wrong")
+        else
+            return print("Successfully created an webhook but I'm unable to print the response because of the lack of features!")
+        end
+    end
+end
