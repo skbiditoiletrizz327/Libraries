218b1fdb99dbc82d7ecaf77e705958c293ab6352
skbiditoiletrizz327 <slomogurl37@gmail.com>
2024-07-08 18:58:57 +0200

    Update Send Message.lua

--- Discord/Functions/Send Message.lua
+++ Discord/Functions/Send Message.lua
@@ -2,41 +2,45 @@ math.randomseed(os.time())
 local WebHookTable = {}
 local Request = request 
 local randomWait = math.random(1)
-WebHookTable.SendMessage = function(Type2,Webhook,Message)
+WebHookTable.SendMessage = function(Type2, Webhook, Message)
     if Webhook ~= "" and Type2 ~= "" then
-        local Content;
-        if Type2 == "Message" then 
+        local Content
+        if Type2 == "Message" then
             Content = {
                 ["content"] = Message
             }
         elseif Type2 == "Embed" then
             Content = {
-                ["content"] = Message.Content, 
+                ["content"] = Message.Content,
                 ["embeds"] = {
-                    ["color"] = Message.Color,
-                    ["title"] = Message.Title,
-                    ["description"] = Message.Description,
-                    ["footer"] = {
-                        ["text"] = Message.Footer 
-                    },
-                    ["author"] = {
-                        ["name"] = Message.Author,
-                        ["icon_url"] = Message.Icon
+                    {
+                        ["color"] = Message.Color,
+                        ["title"] = Message.Title,
+                        ["description"] = Message.Description,
+                        ["footer"] = {
+                            ["text"] = Message.Footer
+                        },
+                        ["author"] = {
+                            ["name"] = Message.Author,
+                            ["icon_url"] = Message.Icon
+                        }
                     }
                 }
             }
         end
-        local respone = Request({
+
+        local response = Request({
             Url = Webhook,
             Method = "POST",
             Headers = {
                 ["Content-Type"] = "application/json"
             },
-            Body = game:GetService("HttpService"):JSONEncode(Content) 
+            Body = game:GetService("HttpService"):JSONEncode(Content)
         })
-        if respone.StatusCode == 204 then
+
+        if response.StatusCode == 204 then
             print("Success!")
-        elseif respone.StatusCode ~= 204 then
+        else
             print("Something didn't work")
         end
     end
